7cf1008e7e1c1aa2db2f4f69fe00b3543f0f1d2b
minh duc nguyen <duc.nguyenminh@ascendcorp.com>
2025-11-13 11:39:33 +0700

    init first backend service

---
+++ backend/auth-srv/src/main/java/com/example/j2n/AuthSrvApplication.java
@@ -0,0 +1,13 @@
+package com.example.j2n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSrvApplication.class, args);
+	}
+
+}
