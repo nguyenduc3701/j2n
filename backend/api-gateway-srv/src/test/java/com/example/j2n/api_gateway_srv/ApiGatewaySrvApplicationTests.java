5c1c48822f64f4a8b4c952b2278763eef7ff7ed8
minh duc nguyen <duc.nguyenminh@ascendcorp.com>
2025-11-24 15:25:32 +0700

    init api-gateway service source

---
+++ backend/api-gateway-srv/src/test/java/com/example/j2n/api_gateway_srv/ApiGatewaySrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.j2n.api_gateway_srv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewaySrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
