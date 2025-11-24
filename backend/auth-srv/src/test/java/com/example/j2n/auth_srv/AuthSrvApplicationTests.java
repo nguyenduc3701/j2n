714f879750de1849f4529781d4f2c959818a085e
minh duc nguyen <duc.nguyenminh@ascendcorp.com>
2025-11-24 15:46:41 +0700

    init auth service source

---
+++ backend/auth-srv/src/test/java/com/example/j2n/auth_srv/AuthSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.j2n.auth_srv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
