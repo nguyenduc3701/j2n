aea9337f2c856158bbb5d29d3143cb0e2288050b
minh duc nguyen <duc.nguyenminh@ascendcorp.com>
2026-01-07 18:33:56 +0700

    update security and controllers

--- backend/image-srv/src/main/java/com/example/j2n/image_srv/controller/ImageControllers.java
+++ backend/image-srv/src/main/java/com/example/j2n/image_srv/controller/ImageControllers.java
@@ -6,6 +6,7 @@ import com.example.j2n.image_srv.service.ImageService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class ImageControllers {
     private final ImageService imageService;
 
-    @PostMapping(value = "/upload", consumes = "multipart/form-data")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "Upload image", description = "Upload image")
     public ImageEntity uploadImage(UploadImageRequest request) {
         return imageService.uploadImage(
