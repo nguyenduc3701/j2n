6eaa52696d23231f2781299b892d9dbb3a5134de
minh duc nguyen <duc.nguyenminh@ascendcorp.com>
2026-03-03 17:20:02 +0700

    update unupdateable config list

--- backend/config-srv/src/main/java/com/example/j2n/config_srv/constant/ConfigConstants.java
+++ backend/config-srv/src/main/java/com/example/j2n/config_srv/constant/ConfigConstants.java
@@ -12,7 +12,27 @@ public class ConfigConstants {
 
     private static final Set<String> UNUPDATABLE_CONFIG_KEYS = Set.of(
             "spring.datasource.password",
-            "spring.cloud.vault.token");
+            "spring.cloud.vault.token",
+            "application.rabbitmq.host",
+            "application.rabbitmq.port",
+            "application.rabbitmq.username",
+            "application.rabbitmq.password",
+            "application.database.base-url",
+            "application.database.username",
+            "application.database.password",
+            "application.redis.host",
+            "application.redis.port",
+            "application.redis.password",
+            "application.api-gateway.base-url",
+            "application.config.base-url",
+            "api-gateway.auth.base-url",
+            "api-gateway.report.base-url",
+            "api-gateway.image.base-url",
+            "api-gateway.bff.base-url",
+            "image.minio.url",
+            "image.minio.access-key",
+            "image.minio.secret-key",
+            "bff.base-url");
 
     public static boolean isUnupdatable(String configKey) {
         return UNUPDATABLE_CONFIG_KEYS.contains(configKey);
